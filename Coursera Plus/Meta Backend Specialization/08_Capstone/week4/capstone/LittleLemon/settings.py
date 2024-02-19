3a22575d340de8337f7968e9bf0f9094829cd11a
alkhaksari@intracom-telecom.com <alireza_kfz@yahoo.com>
2024-02-19 16:12:08 +0200

    Backend Capstone

--- Coursera Plus/Meta Backend Specialization/08_Capstone/week4/capstone/LittleLemon/settings.py
+++ Coursera Plus/Meta Backend Specialization/08_Capstone/week4/capstone/LittleLemon/settings.py
@@ -79,17 +79,23 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'menu_items',
+#         'HOST' :  '192.168.1.10', #'127.0.0.1',
+#         'PORT' : '3306',
+#         'USER' : 'admindjango',#'root',
+#         'PASSWORD' : 'employee@123!',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'menu_items',
-        'HOST' :  '192.168.1.10', #'127.0.0.1',
-        'PORT' : '3306',
-        'USER' : 'admindjango',#'root',
-        'PASSWORD' : 'employee@123!',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
